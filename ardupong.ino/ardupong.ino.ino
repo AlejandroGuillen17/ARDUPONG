835a0d90755bcc04cf97512cd494be74a67107e2
alejandro guillen <67745590+AlejandroGuillen17@users.noreply.github.com>
2022-07-03 20:36:03 -0300

    Add files via upload

---
+++ ardupong.ino/ardupong.ino.ino
@@ -0,0 +1,17 @@
+// Definimos los pines de los dos potenciómetros
+const int potPin1=A2;
+const int potPin2=A0;
+ 
+void setup(){
+  Serial.begin(9600); // Inicializamos el puerto serie
+}
+
+void loop(){
+  
+  String movimientos = ""; // Definimos la variable movimientos para almacenar la lectura de los potenciómetros
+  movimientos += analogRead(potPin1); // Leemos el valor del pin de la entrada analógica 1 y lo agregamos a la variable movimientos
+  movimientos += '-'; // Agregamos un guión a la variable movimientos para separar los valores de los dos pines
+  movimientos += analogRead(potPin2); // Leemos el valor del pin de la entrada analógica 2 y lo agregamos a la variable movimientos
+  Serial.println(movimientos); // Enviamos el valor de la variable movimientos a Processing a través del puerto serial
+  delay(100);
+}
